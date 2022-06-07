497856d05e74101230c984537c85783b99f95bf0
Silvio Cardoso <silvio.cardoso@Silvio-MacBook-Pro.local>
2022-06-07 14:27:50 +0200

    format current situation

--- javascript/index.js
+++ javascript/index.js
@@ -3,11 +3,13 @@ function calcTimeIsMoney(salary, travelTimeMin){
     var youPriceHour = salary/40/5
     var travelTimeHour = travelTimeMin/60
     var moneyOnComuting = youPriceHour *travelTimeHour*20*2
+    var travelM = travelTimeHour*20*2
 
     return {
         priceHour: youPriceHour,
         priceTravelTimeHour: travelTimeHour,
-        commutingMoneyConversion: moneyOnComuting
+        commutingMoneyConversion: moneyOnComuting,
+        travelMonth: travelM
     }
 }
 
@@ -17,5 +19,44 @@ function calcGasolinePrice(price){
     return price * average
 }
 
-console.log(calcTimeIsMoney(3000,30))
-console.log(calcGasolinePrice(2.14))
+
+function formatInputWithJavascript(){
+    var salary = 3000;
+    var travelTimeMin = 30;
+    var gasoline = 2.14;
+    var distance = 10;
+
+    const money = calcTimeIsMoney(salary,travelTimeMin)
+    const gasolineExpenses = calcGasolinePrice(gasoline)
+    var elem = document.getElementById('current_situation');
+    elem.innerHTML = '';
+    const formated = `<div>
+            <h6>Travel Time</h3>
+                <p>${travelTimeMin} minutes</p>
+        </div>
+        <div>
+            <h6>Distance</h3>
+                <p>${distance} km</p>
+        </div>
+        <div>
+            <h6>Hours Spent (monthly)</h3>
+                <p>${money.travelMonth} hour</p>
+        </div>
+        <div>
+            <h6>Time conversation in (euro)</h3>
+                <p>YOUR PRICE HOUR</p>
+                <p>${money.priceHour} &euro;</p>
+        </div>
+        <div>
+            <h6>Total</h3>
+                <p>GASOLINE EXPENSES</p>
+                <p>${gasolineExpenses}</p>
+        </div>
+        <div>
+            <h6>Distance from Veloh</h3>
+                <p>GASOLINE EXPENSES</p>
+                <p>0 m</p>
+        </div>
+        </div>`
+    elem.innerHTML = formated
+}
