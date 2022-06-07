80d347d6675108404669d5fa8f2c01b0dd958a39
Silvio Cardoso <silvio.cardoso@Silvio-MacBook-Pro.local>
2022-06-07 15:13:41 +0200

    conditional colors

--- javascript/index.js
+++ javascript/index.js
@@ -117,25 +117,25 @@ function formatInputWithJavascript(){
     elem.innerHTML = '';
     const formated = `<div>
         <h6>Travel Time</h3>
-        <p>${travelTimeMin} minutes</p>
+        <p ${(travelTimeMin>20) ? 'id="text_on_red"' : 'id="text_on_black"'} >${travelTimeMin} minutes</p>
         </div>
         <div>
             <h6>Distance</h3>
-                <p>${distance} km</p>
+                <p ${(distance>15) ? 'id="text_on_red"' : 'id="text_on_black"'}>${distance} km</p>
         </div>
         <div>
             <h6>Hours Spent (monthly)</h3>
-                <p>${money.travelMonth} hour</p>
+                <p ${(money.travelMonth>40) ? 'id="text_on_red"' : 'id="text_on_black"'}>${money.travelMonth} hour</p>
         </div>
         <div>
             <h6>Time conversation in (euro)</h3>
                 <p>YOUR PRICE HOUR</p>
-                <p>${money.priceHour} &euro;</p>
+                <p ${(money.priceHour>10) ? 'id="text_on_green"' : 'id="text_on_black"'}>${money.priceHour} &euro;</p>
         </div>
         <div>
             <h6>Total</h3>
                 <p>GASOLINE EXPENSES</p>
-                <p>${gasolineExpenses}</p>
+                <p ${(gasolineExpenses>0) ? 'id="text_on_red"' : 'id="text_on_black"'}>${gasolineExpenses}</p>
         </div>
         <div>
             <h6>Distance from Veloh</h3>
@@ -143,4 +143,8 @@ function formatInputWithJavascript(){
         </div>
     <div>`
     elem.innerHTML = formated
+}
+
+function getVelohDistance(){
+    const API_KEY = '51e4c8697d96ebce32a43ec1f34625abb0598e0f'
 }
